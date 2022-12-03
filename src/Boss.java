fb218686a050e473ce599adba906af865714a967
Andrew Zola <and.zola.77@gmail.com>
2022-12-03 16:48:43 +0100

    Commit with Queue and Deque

---
+++ src/Boss.java
@@ -0,0 +1,13 @@
+import java.util.Deque;
+import java.util.Queue;
+
+public class Boss {
+    private Deque toDoDeque;// у босса тоже есть ссылка на очередь из заданий
+    public Boss(Deque toDoDeque) {
+        this.toDoDeque = toDoDeque;// даем ссылку на очередь в конструкторе
+    }
+    public void giveTask(String task) {
+        toDoDeque.push(task); // кладем задание в очередь
+        System.out.println("Дал задачу " + task);
+    }
+}
